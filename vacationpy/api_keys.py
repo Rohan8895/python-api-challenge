9d90e8093e3b52268ff2d57c68e88ec8f8079423
Rohan <rohan@Rohans-MBP-2.fios-router.home>
2020-01-26 23:07:09 -0600

    hw

--- vacationpy/api_keys.py
+++ vacationpy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "4233545a55cf2bfa4cf9fdd1e96db44e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDVKPsLd3tOaX_AVg5ulH59uVRNcGKQ1ck"
---
+++ weatherpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "4233545a55cf2bfa4cf9fdd1e96db44e"
+
+# Google API Key
+g_key = "AIzaSyDVKPsLd3tOaX_AVg5ulH59uVRNcGKQ1ck"
